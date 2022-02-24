85e9833d67ba39bbdacdfeb345c550191be81c03
MarcCanellasG <marccg8@gmail.com>
2022-02-24 17:30:25 +0100

    nivel1

--- Nivel1/bloques.c
+++ Nivel1/bloques.c
@@ -15,6 +15,7 @@ int bmount(const char *camino){
 
     if(descriptor==-1){
 
+        perror("Error al abrir el fichero");
         return EXIT_FAILURE;
     }
     else{
@@ -63,11 +64,13 @@ int bwrite(unsigned int nbloque, const void *buf){
 
         }else{
 
+            perror("Error en la escritura");
             return EXIT_FAILURE;
         }
 
     }else{
 
+        perror("Error en el posicionamiento");
         return EXIT_FAILURE;
     }
 
@@ -93,11 +96,13 @@ int bread(unsigned int nbloque, void *buf){
             return b;
         }else{
 
+            perror("Error en la lectura");
             return EXIT_FAILURE;
         }
 
     }else{
         
+        perror("Error en el posicionamiento");
         return EXIT_FAILURE;
     }
 
