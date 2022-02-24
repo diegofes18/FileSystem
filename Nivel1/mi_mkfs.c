2654b36e18ef0b2120aa953a4ccd8bdb49c7d4d4
diegofes18 <bermejodiego1@gmail.com>
2022-02-24 17:27:40 +0100

    nivel1

--- Nivel1/mi_mkfs.c
+++ Nivel1/mi_mkfs.c
@@ -2,18 +2,7 @@
 
 char* buf;
 
-// int main(int argc, char **argv){
-//     int fd;
-//     char* buf;
 
-//     fd=bmount(argv[1]);
-//     memset(&buf,0,BLOCKSIZE);
-    
-//     for(int i=0;argv[2];i++){
-//         bwrite(i,&buf);
-//     }
-//     bumount();
-// }
 int main(int argc, char **argv){
 
     //Mirar la sintaxis
@@ -24,5 +13,32 @@ int main(int argc, char **argv){
     //Obtener parametros del comando
     char *camino=argv[1];
     int nbloques=atoi(argv[2]);
-    
+
+    //iniciamos el buffer
+    char buf[BLOCKSIZE];
+    memset(buf, 0, BLOCKSIZE);
+
+
+    //Montamos y comprobamos
+    if(bmount(camino)==-1) {
+        perror("Error el montar \n");
+        return EXIT_FAILURE;
+    }
+
+    //Escribimos en el dispositivo
+    for(int i=0; i<nbloques; i++){
+        if(bwrite(i,buf)==EXIT_FAILURE){
+            perror("Error al escribir en el dispostivo virtual");
+            return EXIT_FAILURE;
+        }
+    }
+    //Desmontamos el dispostivo virtual
+     if (bumount() == EXIT_FAILURE)
+    {
+        perror("Error al desmontar\n"); 
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS; 
+
+
 }
