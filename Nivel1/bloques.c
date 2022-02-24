d1605a1683d016144fc008baeed5809ddd9712f6
diegofes18 <bermejodiego1@gmail.com>
2022-02-24 16:13:49 +0100

    main

--- Nivel1/bloques.c
+++ Nivel1/bloques.c
@@ -1,1 +1,14 @@
 #include "bloques.h"
+
+int bmount(const char *camino){
+
+}
+int bumount(){
+
+}
+int bwrite(unsigned int nbloque, const void *buf){
+
+}
+int bread(unsigned int nbloque, void *buf){
+
+}
--- Nivel1/mi_mkfs.c
+++ Nivel1/mi_mkfs.c
@@ -1,1 +1,28 @@
 #include "bloques.h"
+
+char* buf;
+
+// int main(int argc, char **argv){
+//     int fd;
+//     char* buf;
+
+//     fd=bmount(argv[1]);
+//     memset(&buf,0,BLOCKSIZE);
+    
+//     for(int i=0;argv[2];i++){
+//         bwrite(i,&buf);
+//     }
+//     bumount();
+// }
+int main(int argc, char **argv){
+
+    //Mirar la sintaxis
+    if(argc != 3){
+        perror("Sintaxis incorrecta -> ./mi_fks <nombre del fichero> <numero de bloques>\n");
+        return EXIT_FAILURE;
+    }
+    //Obtener parametros del comando
+    char *camino=argv[1];
+    int nbloques=atoi(argv[2]);
+    
+}
