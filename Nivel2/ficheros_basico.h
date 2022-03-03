4e312687ffcb8147c30f6c0e04ce8cc4929d5cd6
diegofes18 <bermejodiego1@gmail.com>
2022-03-03 12:20:16 +0100

    cosas2

--- Nivel2/ficheros_basico.h
+++ Nivel2/ficheros_basico.h
@@ -18,6 +18,39 @@ struct superbloque {
    unsigned int totInodos;                              // Cantidad total de inodos
    char padding[BLOCKSIZE - 12 * sizeof(unsigned int)]; // Relleno
 };
+
+struct inodo{
+   
+   char tipo;
+   char permisos
+
+    /* Por cuestiones internas de alineación de estructuras, si se está utilizando
+    un tamaño de palabra de 4 bytes (microprocesadores de 32 bits):
+   unsigned char reservado_alineacion1 [2];
+   en caso de que la palabra utilizada sea del tamaño de 8 bytes
+   (microprocesadores de 64 bits): unsigned char reservado_alineacion1 [6]; */
+   unsigned char reservado_alineacion1[6];
+ 
+   time_t atime; // Fecha y hora del último acceso a datos: atime
+   time_t mtime; // Fecha y hora de la última modificación de datos: mtime
+   time_t ctime; // Fecha y hora de la última modificación del inodo: ctime
+ 
+   /* comprobar el tamaño del tipo time_t para vuestra plataforma/compilador:
+   printf ("sizeof time_t is: %d\n", sizeof(time_t)); */
+ 
+   unsigned int nlinks;             // Cantidad de enlaces de entradas en directorio
+   unsigned int tamEnBytesLog;      // Tamaño en bytes lógicos
+   unsigned int numBloquesOcupados; // Cantidad de bloques ocupados zona de datos
+ 
+   unsigned int punterosDirectos[12];  // 12 punteros a bloques directos
+   unsigned int punterosIndirectos[3]; /* 3 punteros a bloques indirectos:
+   1 indirecto simple, 1 indirecto doble, 1 indirecto triple */
+ 
+   /* Utilizar una variable de alineación si es necesario  para vuestra plataforma/compilador   */
+   char padding[INODOSIZE - 2 * sizeof(unsigned char) - 3 * sizeof(time_t) - 18 * sizeof(unsigned int) - 6 * sizeof(unsigned char)];
+   // Hay que restar también lo que ocupen las variables de alineación utilizadas!!!
+};
+
 int tamMB(unsigned int nbloques);
 int tamAI(unsigned int ninodos);
 int initSB(unsigned int nbloques, unsigned int ninodos);
