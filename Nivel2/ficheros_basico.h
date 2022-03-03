ecdf20101181650b5334319c7b55061b0f7f6371
diegofes18 <bermejodiego1@gmail.com>
2022-03-03 12:00:14 +0100

    basico.h

--- Nivel2/ficheros_basico.h
+++ Nivel2/ficheros_basico.h
@@ -1,3 +1,10 @@
 #include <bloques.h>
 
+#define posSB 0 // el superbloque se escribe en el primer bloque de nuestro FS
+#define tamSB 1
 
+int tamMB(unsigned int nbloques);
+int tamAI(unsigned int ninodos);
+int initSB(unsigned int nbloques, unsigned int ninodos);
+int initMB(); 
+int initAI();
