74e608b5242e7d1eaf6e434fd8bbbca265ab6529
diegofes18 <bermejodiego1@gmail.com>
2022-03-10 16:10:49 +0100

    mivel3

--- Nivel3/leer_sf.c
+++ Nivel3/leer_sf.c
@@ -92,6 +92,56 @@ int main(int argc, char const *argv[])
             contlibres++;
         }
     }
+     //Probación el tamaño del tipo time_t para vuestra plataforma/compilador:
+    //printf("\nsizeof time_t is: %ld\n", sizeof(time_t));
+
+    printf("\nRESERVAMOS UN BLOQUE Y LUEGO LO LIBERAMOS:\n");
+    int reservado = reservar_bloque(); // Actualiza el SB
+    bread(posSB, &SB);                 // Actualizar los valores del SB
+
+    printf("Se ha reservado el bloque físico nº %i que era el 1º libre indicado por el MB.\n", reservado);
+    printf("SB.cantBloquesLibres: %i\n", SB.cantBloquesLibres);
+    liberar_bloque(reservado);
+    bread(posSB, &SB); // Actualizar los valores del SB
+
+    printf("Liberamos ese bloque, y después SB.cantBloquesLibres: %i\n\n", SB.cantBloquesLibres);
+    printf("MAPA DE BITS CON BLOQUES DE METADATOS OCUPADOS\n");
+    int bit = leer_bit(posSB);
+    printf("leer_bit(%i) = %i\n", posSB, bit);
+    bit = leer_bit(SB.posPrimerBloqueMB);
+    printf("leer_bit(%i) = %i\n", SB.posPrimerBloqueMB, bit);
+    bit = leer_bit(SB.posUltimoBloqueMB);
+    printf("leer_bit(%i) = %i\n", SB.posUltimoBloqueMB, bit);
+    bit = leer_bit(SB.posPrimerBloqueAI);
+    printf("leer_bit(%i) = %i\n", SB.posPrimerBloqueAI, bit);
+    bit = leer_bit(SB.posUltimoBloqueAI);
+    printf("leer_bit(%i) = %i\n", SB.posUltimoBloqueAI, bit);
+    bit = leer_bit(SB.posPrimerBloqueDatos);
+    printf("leer_bit(%i) = %i\n", SB.posPrimerBloqueDatos, bit);
+    bit = leer_bit(SB.posUltimoBloqueDatos);
+    printf("leer_bit(%i) = %i\n", SB.posUltimoBloqueDatos, bit);
+    printf("\nDATOS DEL DIRECTORIO RAIZ\n\n");
+    struct tm *ts;
+    char atime[80];
+    char mtime[80];
+    char ctime[80];
+
+    struct inodo inodo;
+    int ninodo = 0; //el directorio raiz es el inodo 0
+    leer_inodo(ninodo, &inodo);
+    ts = localtime(&inodo.atime);
+    strftime(atime, sizeof(atime), "%a %Y-%m-%d %H:%M:%S", ts);
+    ts = localtime(&inodo.mtime);
+    strftime(mtime, sizeof(mtime), "%a %Y-%m-%d %H:%M:%S", ts);
+    ts = localtime(&inodo.ctime);
+    strftime(ctime, sizeof(ctime), "%a %Y-%m-%d %H:%M:%S", ts);
+    printf("tipo: %c\n", inodo.tipo);
+    printf("permisos: %i\n", inodo.permisos);
+    printf("ID: %d \nATIME: %s \nMTIME: %s \nCTIME: %s\n", ninodo, atime, mtime, ctime);
+    printf("nlinks: %i\n", inodo.nlinks);
+    printf("tamaño en bytes lógicos: %i\n", inodo.tamEnBytesLog);
+    printf("Número de bloques ocupados: %i\n", inodo.numBloquesOcupados);
+
 
        // Desmonta el disco del sistema.
        bumount();
